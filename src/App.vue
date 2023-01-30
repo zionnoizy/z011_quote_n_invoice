5459cc21913aed9f38526ce5685a90b21765742f
zionnoizy <zion95123@outlook.com>
2023-01-30 10:05:01 +0000

    add fb

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { db } from '@/firebase'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    (v0.01-Iron Barbs)
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
---
+++ src/firebase/index.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmnk75XdGraJ2LbDC6VQAM-_J5wkwCAtw",
+  authDomain: "quote-n-invoice.firebaseapp.com",
+  projectId: "quote-n-invoice",
+  storageBucket: "quote-n-invoice.appspot.com",
+  messagingSenderId: "183239941114",
+  appId: "1:183239941114:web:001bd5427fcab85a6ce243"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+    db
+}
